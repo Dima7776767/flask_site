439dafd42423e6d19cd760f8ddc3a369f4c69f9c
Dima7776767 <138670505+Dima7776767@users.noreply.github.com>
2025-04-02 21:56:56 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,11 @@
+from setuptools import setup, find_packages
 
+setup(
+    name="flask_site",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        'flask',
+        'gunicorn',
+    ],
+)
